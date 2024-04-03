3c2609876a37b8ca455fba11ce4c43ccbdaf7d0d
EssiJunior <nessipjunior@gmail.com>
2024-04-03 14:58:59 +0200

    Presentable

--- src/pages/InProgress.jsx
+++ src/pages/InProgress.jsx
@@ -18,7 +18,7 @@ const InProgress = ({theme}) => {
     return (
         <main className='doing' style={{backgroundColor:`${theme.colors.bg}`, color:`${theme.colors.text}`}}>
             <div className="wrapper">
-                <AnimationLottie animationPath={doing} width='40%'/>
+                <AnimationLottie animation={doing} width='40%'/>
                 <Typography text='In development...' />
                 <Link to='/'><Typography text='< Go to home' className='blue-text-gradient' /></Link>
             </div>
