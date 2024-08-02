0b6bd4d8bb29467214e5dd1ba870295a4ec71ca5
EssiJunior <nessipjunior@gmail.com>
2024-08-02 11:02:37 +0100

    Updated projects

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,7 +1,8 @@
 import flytravel from './flytravel.png'
 import flikart from './flikart.png'
+import cara from './cara.png'
 import portfolio from './portfolio.png'
 import proservice from './proservice.png'
 import recho from './recho.png'
 
-export {flikart, proservice, flytravel, portfolio, recho} 
+export {flikart, proservice, flytravel, portfolio, recho, cara} 
