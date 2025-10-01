edd2038adeaddb0231d0a1cb90a94957e92dc426
EssiJunior <nessipjunior@gmail.com>
2025-10-01 16:10:02 +0100

    Css minifyer set

--- vite.config.js
+++ vite.config.js
@@ -17,25 +17,8 @@ export default defineConfig({
         exclude: ['js-big-decimal']
     },
     build: {
-        rollupOptions: {
-            output: {
-                // IMPORTANT: Ajouter des hash aux fichiers
-                assetFileNames: (assetInfo) => {
-                    let extType = assetInfo.name.split('.').pop();
-                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                        extType = 'img';
-                    } else if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
-                        extType = 'fonts';
-                    }
-                    return `assets/${extType}/[name]-[hash][extname]`;
-                },
-                chunkFileNames: 'assets/js/[name]-[hash].js',
-                entryFileNames: 'assets/js/[name]-[hash].js',
-            },
-        },
-        // Générer le manifest pour le cache busting
-        manifest: true,
-        // Optimiser les chunks
-        chunkSizeWarningLimit: 1000,
-    },
+        cssMinify: 'lightningcss',
+        minify: 'esbuild',
+        cssCodeSplit: true,
+    }
 })
