89786837b27f8a172c6e21790165638cff7bbd2b
EssiJunior <nessipjunior@gmail.com>
2026-02-16 06:39:04 +0100

    Created Heading overlays

--- src/components/Overlays/PrimaryHeadingOverlay.jsx
+++ src/components/Overlays/PrimaryHeadingOverlay.jsx
@@ -1,8 +1,8 @@
 
-const TitleOverlay = () => {
+const PrimaryHeadingOverlay = () => {
     return (
         <div className="w-[100px] h-[100px] bg-violet-200 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
     )
 }
 
-export default TitleOverlay
+export default PrimaryHeadingOverlay
---
+++ src/components/Overlays/SecondaryHeadingOverlay.jsx
@@ -0,0 +1,15 @@
+import bg from '@/assets/utilities/section.svg'
+
+const SecondaryHeadingOverlay = () => {
+    return (
+        <img
+            loading="lazy"
+            src={bg}
+            alt="Hero"
+            width={1572}
+            height={795}
+            className="absolute top-0 -z-10"
+        />)
+}
+
+export default SecondaryHeadingOverlay
